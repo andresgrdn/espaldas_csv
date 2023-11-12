075c26f63ec2f7a4473014ce37e1c59693d5e537
Andres Gordon <gordon_andres@yahoo.es>
2023-11-12 12:35:00 -0500

    ✅ [UPDATE] result es un string

--- tests-main.py
+++ tests-main.py
@@ -6,7 +6,7 @@ class PruebaMyLayout(unittest.TestCase):
     def test_nombre_y_numero(self):
         myLayout = MyLayout()
         result = myLayout.parse_text('Andres 12')
-        self.assertEqual(result, ('ANDRES', 12))
+        self.assertEqual(result, ('ANDRES', '12'))
 
     def test_nombre_sin_numero(self):
         myLayout = MyLayout()
@@ -16,7 +16,7 @@ class PruebaMyLayout(unittest.TestCase):
     def test_numero_sin_nombre(self):
         myLayout = MyLayout()
         result = myLayout.parse_text('12')
-        self.assertEqual(result, ('BORRAR', 12))
+        self.assertEqual(result, ('BORRAR', '12'))
 
     def test_sin_nombre_y_numero(self):
         myLayout = MyLayout()
@@ -26,12 +26,12 @@ class PruebaMyLayout(unittest.TestCase):
     def test_nombre_compuesto_y_numero(self):
         myLayout = MyLayout()
         result = myLayout.parse_text('Andres g. 12')
-        self.assertEqual(result, ('ANDRES G.', 12))
+        self.assertEqual(result, ('ANDRES G.', '12'))
 
     def test_nombre_compuesto_con_numero_y_numero(self):
         myLayout = MyLayout()
         result = myLayout.parse_text('Andres12 G. 12')
-        self.assertEqual(result, ('ANDRES12 G.', 12))
+        self.assertEqual(result, ('ANDRES12 G.', '12'))
 
 
 if __name__ == '__main__':
